6d5053a0c007e2f7bcb6b49f8aa04a3241562013
chairulazmi13 <mkhairulazmi13@gmail.com>
2019-04-20 11:49:11 +0700

    delete widget bar:on develop on brach dashboard

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -86,27 +86,6 @@
             <!-- /.box-body -->
           </div>
         </div>
-        <div class="col-md-6">
-          <!-- BAR CHART -->
-          <div class="box box-success">
-            <div class="box-header with-border">
-              <h3 class="box-title">Bar Chart</h3>
-
-              <div class="box-tools pull-right">
-                <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i>
-                </button>
-                <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-              </div>
-            </div>
-            <div class="box-body">
-              <div class="chart">
-                <canvas id="barChart" style="height:230px"></canvas>
-              </div>
-            </div>
-            <!-- /.box-body -->
-          </div>
-          <!-- /.box -->
-        </div>
       </div>
 
     </section>
