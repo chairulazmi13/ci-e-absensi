ae4b939d2d5a93844fb581d0fa359159f068d118
chairulazmi13 <mkhairulazmi13@gmail.com>
2019-04-20 09:20:30 +0700

    new function createIdCuti: id cuti otomatis

--- application/models/mCuti.php
+++ application/models/mCuti.php
@@ -2,6 +2,26 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class mCuti extends CI_Model {
+
+  function createIdCuti()
+  {
+      $this->db->select('RIGHT(cuti.id_cuti,17) as kode', FALSE);
+      $this->db->order_by('id_cuti','DESC');    
+      $this->db->limit(1);    
+      $query = $this->db->get('cuti');      //cek dulu apakah ada sudah ada kode di tabel.    
+      if($query->num_rows() <> 0){      
+       //jika kode ternyata sudah ada.      
+       $data = $query->row();      
+       $kode = intval($data->kode) + 1;    
+      }
+      else {      
+       //jika kode belum ada      
+       $kode = 1;    
+      }
+      $kodemax = str_pad($kode, 4, "0", STR_PAD_LEFT); // angka 4 menunjukkan jumlah digit angka 0
+      $kodejadi = $kodemax."/PML/CUTI/".date('m')."/".date('Y');    // hasilnya ODJ-9921-0001 dst.
+      return $kodejadi;  
+  }
  
   function getAll(){
     $this->db->select('id_pegawai,nip,nama,kota,alamat,id_cuti,tanggal_pengajuan,tanggal_mulai,tanggal_selesai,jumlah_hari,jenis_cuti,keterangan,approve,approve_by,file');
