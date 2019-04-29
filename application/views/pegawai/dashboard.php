fbe753c15335e57fe86594f03eca22341b1fb1cd
chairulazmi13 <mkhairulazmi13@gmail.com>
2019-04-29 11:22:51 +0700

    update qr panel

--- application/views/pegawai/dashboard.php
+++ application/views/pegawai/dashboard.php
@@ -14,16 +14,21 @@
       <div class="row">
         <div class="col-md-4">
           <div class="box box-success">
-            <div class="box-header"><h4>QR Code untuk absensi</h4></div>
+            <div class="box-header"><h4>QR Code</h4></div>
             <div class="box-body">
-              <div id="qr-code"></div>
+              <div id="qr-code">
+                <div class="callout callout-warning">
+                  <h4>Info</h4>
+                  <p>Lakukan Generate setiap kali melakukan Absensi</p>
+                </div>
+              </div>
             </div>
             <div class="box-footer">
               <button class="btn btn-success btn-lg btn-block" id="Generate"><i class="fa fa-fw fa-qrcode"></i> Generate QR Code</button>
             </div>
           </div>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-8">
           <div class="box box-primary">
            <div class="box-header with-border">
              <h4 class="box-title">Index Kehadiran Bulan ini</h4>
@@ -72,10 +77,11 @@
                 data: {id_pegawai: id_pegawai,nip:nip},
                 dataType:'json',
                 beforeSend:function(){
-                    $('#qr-code').html('<span class="text-info">Loading response...</span>');
+                    $('#Generate').html('Loading response... <i class="fa fa-circle-o-notch fa-spin fa-fw">');
                 },
                 success: function(data) {
-                    console.log(data['img'])
+                    console.log(data['img']);
+                    $('#Generate').html('<i class="fa fa-fw fa-qrcode"></i> Generate QR Code');
                     $('#qr-code').fadeIn().html('<img class="img-responsive pad" alt="photo" style="margin: 0 auto;" src="<?=base_url()?>'+data['img']+'">');
                 }
             });
